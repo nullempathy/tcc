cdafaf1e602333dd8119b97bacf5a372fb54086e
Igor Gabriel <138086755+acato21@users.noreply.github.com>
2024-12-12 06:39:31 -0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,25 +5,28 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" href="/favicon.ico" sizes="15x25" />
-        
+        {/* Script do Google Tradutor carregado de forma assíncrona */}
         <script
           type="text/javascript"
           src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
-        />
+          async
+        ></script>
       </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
 
+        {/* Div para o Google Tradutor */}
         <div id="google_translate_element"></div>
 
+        {/* Função de inicialização do tradutor */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
               function googleTranslateElementInit() {
                 new google.translate.TranslateElement({
-                  pageLanguage: 'en', // Idioma original da página (Inglês)
-                  includedLanguages: 'pt', // Idioma disponível (Português)
+                  pageLanguage: 'en', // Idioma original da página
+                  includedLanguages: 'pt', // Idioma disponível
                   layout: google.translate.TranslateElement.InlineLayout.SIMPLE
                 }, 'google_translate_element');
               }
