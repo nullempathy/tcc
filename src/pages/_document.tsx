2290be08f93a4c78c083e5f789ef6e360004c6f1
Igor Gabriel <138086755+acato21@users.noreply.github.com>
2024-12-12 06:34:34 -0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,14 +2,38 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="pt-br">
+    <Html lang="en">
       <Head>
         <title>Confia Primo</title>
         <link rel="icon" href="/favicon.ico" sizes="15x25" />
+        
+        {/* Script do Google Tradutor */}
+        <script
+          type="text/javascript"
+          src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+        />
       </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
+
+        {/* Div do Google Tradutor */}
+        <div id="google_translate_element"></div>
+
+        {/* Função de inicialização do tradutor */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              function googleTranslateElementInit() {
+                new google.translate.TranslateElement({
+                  pageLanguage: 'en', // Idioma original da página (Inglês)
+                  includedLanguages: 'pt', // Idioma disponível (Português)
+                  layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+                }, 'google_translate_element');
+              }
+            `,
+          }}
+        />
       </body>
     </Html>
   );
