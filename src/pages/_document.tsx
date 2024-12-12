d5d0c4fad198e0f71c86e2532a4ede8e489d6fb9
Igor Gabriel <138086755+acato21@users.noreply.github.com>
2024-12-12 06:27:31 -0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="pt-br">
       <Head>
         <title>Confia Primo</title>
         <link rel="icon" href="/favicon.ico" sizes="15x25" />
